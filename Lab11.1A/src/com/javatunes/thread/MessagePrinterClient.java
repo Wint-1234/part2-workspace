67ab09632669bc4b626212f0e44df7cf6dad8b8b
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-30 17:40:51 -0400

    Completed Labs up to 11.4

--- Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
+++ Lab11.1A/src/com/javatunes/thread/MessagePrinterClient.java
@@ -11,5 +11,10 @@ public class MessagePrinterClient {
 
   public static void main(String[] args) {
     // TODO: create two instances of your thread subclass and start them up
+    MessagePrinter printer1 = new MessagePrinter("Printer 1.");
+    printer1.start();
+
+    MessagePrinter printer2 = new MessagePrinter("Printer 2.");
+    printer2.start();
   }
 }
